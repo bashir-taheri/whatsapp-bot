ba35e84891b32b7e4222aa5ef0c46b7a078f8320
bashir-taheri <hamgab2026@gmail.com>
2026-07-25 08:05:18 +0430

    Create index.js

---
+++ index.js
@@ -0,0 +1,58 @@
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  DisconnectReason
+} = require("@whiskeysockets/baileys");
+
+const P = require("pino");
+
+async function startBot() {
+  const { state, saveCreds } = await useMultiFileAuthState("auth_info");
+
+  const sock = makeWASocket({
+    auth: state,
+    logger: P({ level: "silent" })
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+
+  sock.ev.on("connection.update", ({ connection, qr, lastDisconnect }) => {
+    if (qr) {
+      console.log("QR:");
+      console.log(qr);
+    }
+
+    if (connection === "open") {
+      console.log("✅ Bot Connected");
+    }
+
+    if (connection === "close") {
+      const shouldReconnect =
+        lastDisconnect?.error?.output?.statusCode !==
+        DisconnectReason.loggedOut;
+
+      if (shouldReconnect) {
+        startBot();
+      }
+    }
+  });
+
+  sock.ev.on("messages.upsert", async ({ messages }) => {
+    const msg = messages[0];
+
+    if (!msg.message || msg.key.fromMe) return;
+
+    const text =
+      msg.message.conversation ||
+      msg.message.extendedTextMessage?.text ||
+      "";
+
+    if (text.toLowerCase() === "سلام") {
+      await sock.sendMessage(msg.key.remoteJid, {
+        text: "سلام 🌹 خوش آمدید."
+      });
+    }
+  });
+}
+
+startBot();
